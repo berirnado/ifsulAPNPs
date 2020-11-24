111ed858c451f2ceb827d4bdba5f7aea4c59f8c6
berirnado <be.zito@hotmail.com>
2020-11-24 16:33:43 -0300

    add view folder

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
+
+app.use(cookieParser());
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', function(req, res, next) {
     res.send('Olá Bernardo')
