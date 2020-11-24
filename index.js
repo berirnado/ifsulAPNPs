b955eba8f45ab84734e30328897d2200dbb22e2c
berirnado <be.zito@hotmail.com>
2020-11-24 17:06:17 -0300

    create add page

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ const express = require('express')
 const app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser')
+var path = require('path')
 
 app.use(cookieParser());
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.set("view engine", "ejs")
+
+app.use(express.static(path.join(__dirname, "public")))
+
 app.get('/', function(req, res, next) {
-    res.send('Olá Bernardo')
+    res.render('index.ejs',{})
 });
+
+app.get('/usuarios', function(req, res, next) {
+    res.render('usuarios.ejs',{ usuarios:[
+        {name: 'Diego', email:'dapporcellis@hotmail.com'},
+        {name: 'Maria', email:'maria@hotmail.com'},
+        {name: 'Tereza', email:'tereza@hotmail.com'},
+        {name: 'Ana', email:'ana@hotmail.com'},
+    ]})
+});
+
+app.get('/add', (req,res, next) => {
+    res.render('adiciona.ejs')
+});
+
+app.post('/add', (req,res, next) => {
+    
+});
+
 
 app.listen(3000, function() {
     console.log('Conexão realizada')
